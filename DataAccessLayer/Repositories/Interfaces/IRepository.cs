f06708ad7c347189fabd881194c47d71c1978fd9
Vi Nguyen <xuanvidz03102003@gmail.com>
2025-09-28 22:51:03 +0700

    add base Clean attritechture layer model

---
+++ DataAccessLayer/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(); // Gets all data.
+        Task<T> GetByIdAsync(int id);      // Gets data by ID.
+        Task CreateAsync(T entity);        // Create a new resource.
+        Task UpdateAsync(T entity);        // Updates existing data.
+        Task DeleteByIdAsync(int id);      // Deletes data by ID.
+    }
+}

f554076f35c79586a0fc1554756a1dc1be407a5a
Luongnd <luongndhe170199@fpt.edu.vn>
2025-09-29 12:15:34 +0700

    firstcommit

--- SapaFoRestRMSAPI/Controllers/WeatherForecastController.cs
+++ SapaFoRestRMSAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SapaFoRestRMSAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchings"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
